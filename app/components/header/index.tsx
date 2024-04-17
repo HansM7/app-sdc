ac0a3a0e3c7fd7157805e81582780779e024e40a
victor828 <veom104@gmail.com>
2024-04-16 22:23:41 -0700

    :bento: [UPDATE] add padding
    add padding to header component in List Element

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -8,9 +8,9 @@ export default function HeaderComponent({
   return (
     <header className='flex p-2 bg-zinc-900 w-[40%] rounded-md bg-opacity-80 mx-auto'>
       <div className='px-4'>
-        <h1 className='p-2'>Home</h1>
+        <h1 className='p-2 py-4'>Home</h1>
       </div>
-      <ul className='flex gap-2 bg-zinc-800 rounded-md w-full justify-around'>
+      <ul className='flex gap-2 bg-zinc-800 rounded-md w-full justify-around p-2'>
         {children}
       </ul>
     </header>
