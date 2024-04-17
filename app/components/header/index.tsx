8654c37a5080967a3f80e1783c11b3e90369cfdb
victor828 <veom104@gmail.com>
2024-04-16 22:13:36 -0700

    :sparkles: [ADD] HEaderComponent
    add Header compnent & LiComponent, added to Home Route

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,1 +1,18 @@
-"use client";
+/** @format */
+
+export default function HeaderComponent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <header className='flex p-2 bg-zinc-900 w-[40%] rounded-md bg-opacity-80 mx-auto'>
+      <div className='px-4'>
+        <h1 className='p-2'>Home</h1>
+      </div>
+      <ul className='flex gap-2 bg-zinc-800 rounded-md w-full justify-around'>
+        {children}
+      </ul>
+    </header>
+  );
+}
