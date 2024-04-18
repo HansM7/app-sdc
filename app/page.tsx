e841afd805f4f28e45c3739ed037576e9a03b2d2
HansM7 <melchorhans5@gmail.com>
2024-04-17 22:26:51 -0500

    Add config responsvie!

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,18 @@ import { homeRef } from "./lib/utils";
 
 export default function Home() {
   return (
-    <div className="relative">
-      <HeaderComponent>
+    <div className="relative bg-zinc-800  ">
+      {/* <HeaderComponent>
         {homeRef.map((ref) => {
           return <LiComponent name={ref.name} idRef={ref.idRef} />;
         })}
-      </HeaderComponent>
+      </HeaderComponent> */}
+
+      <header className="w-full bg-red-300 fixed bottom-0 xl:top-0 h-fit mb-16 xl:mt-16 xl:mb-0">
+        header
+      </header>
+
+      <Index />
       <Index />
     </div>
   );
